10fe654078d80ad76e155345017df62804fb68ae
PS AI MUSIC Projekte <power.p1986@googlemail.com>
2026-09-15 00:11:31 +0200

    Compile against Android API 37 while targeting API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.psaimusic.mediaplayer"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.psaimusic.mediaplayer"

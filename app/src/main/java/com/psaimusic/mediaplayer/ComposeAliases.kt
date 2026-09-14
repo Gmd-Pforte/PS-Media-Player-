67edc3e77aa5386dd81c61412ddc3a1da17f90af
PS AI MUSIC Projekte <power.p1986@googlemail.com>
2026-09-15 00:03:42 +0200

    Remove temporary Compose alias

--- app/src/main/java/com/psaimusic/mediaplayer/ComposeAliases.kt
+++
@@ -1,7 +0,0 @@
-package com.psaimusic.mediaplayer
-
-import androidx.compose.foundation.layout.ColumnScope
-
-// Keeps slot APIs readable while the first UI prototype is still contained in MainActivity.
-// This can be removed once the UI is split into dedicated component files.
-internal typealias Column = ColumnScope
